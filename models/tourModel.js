8a423b47a1530c5474930680a236fcc0ba717dfd
gabrielgreco1 <gabrielargreco@gmail.com>
2024-04-21 00:04:44 -0300

    feat: Implemented mongoDB Middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,7 +10,11 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -20,10 +26,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingQuantity: {
       type: Number,
@@ -35,6 +47,14 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'Discount price (${VALUE}) should be below the regular price',
+      },
     },
     summary: {
       type: String,
@@ -56,16 +76,61 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   },
 );
 
+// mongoose middlewares
 tourSchema.virtual('durantionWeeks').get(function () {
   return this.duration / 7;
 });
+
+// slug is basically a string that we put in the url, normalmente criada com o nome
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create() but not for .supdate()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save document..')
+//   next()
+// })
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next()
+// })
+
+// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  // tourSchema.pre('find', function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+// Post happens after the query is executed
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(docs)
+  next();
+});
+
+// // AGREGATION MIDDLEWARE
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { $secretTour: { $ne: false } }});
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
