c36933dd16940ae36fad1625f66de38f44970e0d
gabrielgreco1 <gabrielargreco@gmail.com>
2024-04-22 17:19:01 -0300

    feat: Error handling for all non existence routes

--- app.js
+++ app.js
@@ -26,5 +26,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handler geral:
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server`
+  })
+})
+
 // 4) START SERVER
 module.exports = app;
