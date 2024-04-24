6e9c9f32c6f30f4747144a24d821b2b809022c7d
gabrielgreco1 <gabrielargreco@gmail.com>
2024-04-24 10:15:20 -0300

    feat!: Error controller

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,11 +26,12 @@ app.use('/api/v1/users', userRouter);
 
 // Handler geral:
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cant find ${req.originalUrl} on this server`
-  })
+  // if we pass any parameters to a next(), express will know there is a error
+  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404))
 })
+
+// Error handling middleware
+app.use()
 
 // 4) START SERVER
 module.exports = app;
